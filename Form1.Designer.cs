b95d0fbde2036a892a2f1c501cab6104539fb629
Erfan SH <amir19731394@gmail.com>
2024-10-01 17:27:31 +0330

    Final...

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace EF_WindowsForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_Fmily = new System.Windows.Forms.TextBox();
+            this.txt_Phone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_searchID = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 219);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(778, 225);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 143);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(266, 72);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(537, 143);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 72);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(695, 54);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 30);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Location = new System.Drawing.Point(10, 11);
+            this.txt_ID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(100, 22);
+            this.txt_ID.TabIndex = 2;
+            this.txt_ID.TextChanged += new System.EventHandler(this.txt_ID_TextChanged);
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(10, 37);
+            this.txt_Name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(100, 22);
+            this.txt_Name.TabIndex = 2;
+            // 
+            // txt_Fmily
+            // 
+            this.txt_Fmily.Location = new System.Drawing.Point(10, 63);
+            this.txt_Fmily.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Fmily.Name = "txt_Fmily";
+            this.txt_Fmily.Size = new System.Drawing.Size(100, 22);
+            this.txt_Fmily.TabIndex = 2;
+            // 
+            // txt_Phone
+            // 
+            this.txt_Phone.Location = new System.Drawing.Point(10, 89);
+            this.txt_Phone.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Phone.Name = "txt_Phone";
+            this.txt_Phone.Size = new System.Drawing.Size(100, 22);
+            this.txt_Phone.TabIndex = 2;
+            this.txt_Phone.TextChanged += new System.EventHandler(this.txt_Phone_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone Number";
+            // 
+            // txt_searchID
+            // 
+            this.txt_searchID.Location = new System.Drawing.Point(571, 54);
+            this.txt_searchID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_searchID.Multiline = true;
+            this.txt_searchID.Name = "txt_searchID";
+            this.txt_searchID.Size = new System.Drawing.Size(118, 28);
+            this.txt_searchID.TabIndex = 4;
+            this.txt_searchID.TextChanged += new System.EventHandler(this.txt_searchID_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Empty";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txt_searchID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Phone);
+            this.Controls.Add(this.txt_Fmily);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.txt_ID);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Conected To The Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_Fmily;
+        private System.Windows.Forms.TextBox txt_Phone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_searchID;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace EF_WindowsForm1
+{
+    public partial class Form1 : Form
+    {
+        db2Entities2 db2 = new db2Entities2();
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+           this.dataGridView1.DataSource = db2.Phone_Books.ToList();           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { //Insert
+
+            Phone_Book PB = new Phone_Book();
+
+
+          
+ //in ghesmat mogheii ke az "try catch" Estefade Mikrdm Eror ra neshon midad vali id 0 dar database darj mikard
+
+           if (txt_ID.Text == string.Empty) { MessageBox.Show("!آیدی نباید خالی باشد", "Eror 401"); }
+           if (txt_Phone.Text == string.Empty) { MessageBox.Show("!شماره نباید خالی باشد", "Eror 401"); } 
+           
+           
+
+           //Khastam Az "Regular Expression" Estefade Konam Vali HAr Kari Kardam Natonstm
+            PB.ID = Convert.ToInt32(txt_ID.Text);                 
+            PB.First_Name = txt_Name.Text;
+            PB.Last_Name = txt_Fmily.Text;
+            PB.Phone_Number = Convert.ToInt64(txt_Phone.Text);
+           
+            db2.Phone_Books.Add(PB);
+
+            db2.SaveChanges();
+
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = db2.Phone_Books.ToList();
+   }
+
+        public void show()
+        {
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = db2.Phone_Books.ToList();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        { //Delete
+
+            int current =Convert.ToInt32( this.dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+            Phone_Book PB= db2.Phone_Books.First(c => c.ID == current);
+
+            db2.Phone_Books.Remove(PB);
+
+            db2.SaveChanges();
+
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = db2.Phone_Books.ToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //update
+            int current = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+            Phone_Book PB = db2.Phone_Books.First(c => c.ID == current);
+
+          //PB.ID = Convert.ToInt32(txt_ID.Text);
+            PB.First_Name = txt_Name.Text;
+            PB.Last_Name = txt_Fmily.Text;
+            PB.Phone_Number = Convert.ToInt64(txt_Phone.Text);
+
+            db2.SaveChanges();
+            
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = db2.Phone_Books.ToList();
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            int current = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            Phone_Book PB = db2.Phone_Books.First(c => c.ID == current);
+
+             txt_ID.Text = PB.ID.ToString();
+             txt_Name.Text= PB.First_Name.ToString();
+             txt_Fmily.Text = PB.Last_Name.ToString(); 
+             txt_Phone.Text= PB.Phone_Number.ToString() ;        
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Select
+
+            try
+            {
+                int currentID = Convert.ToInt32(txt_searchID.Text);
+
+
+                Phone_Book PB = db2.Phone_Books.First(c => c.ID == currentID);
+
+                txt_ID.Text = PB.ID.ToString();
+                txt_Name.Text = PB.First_Name.ToString();
+                txt_Fmily.Text = PB.Last_Name.ToString(); ;
+                txt_Phone.Text = PB.Phone_Number.ToString();
+
+                txt_searchID.Text = string.Empty.Trim();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("!کاربر مورد نظر وجود ندارد","Eror 404");
+                txt_searchID.Text = string.Empty.Trim();
+            }
+
+        }    
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_searchID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_ID_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        { 
+            //Empty
+
+            txt_ID.Text = string.Empty.Trim();
+            txt_Name.Text = string.Empty.Trim();
+            txt_Fmily.Text = string.Empty.Trim();
+            txt_Phone.Text = string.Empty.Trim();
+        }
+
+        private void txt_Phone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
